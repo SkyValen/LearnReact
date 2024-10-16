840fb703b852747b14d1e9230260562e32248acf
NikitaSkorohov <nikitaskoroho14@gmail.com>
2024-10-16 15:20:08 +0300

    gotta fix bugs

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -5,7 +5,7 @@ import './post-list-item.css'
 export default class PostListItem extends Component {
 
     render() {
-        const { label, onDelete, onToggleImportant, onToggleLiked, like, important } = this.props;
+        const {label, onDelete, onToggleImportant, onToggleLiked, like, important } = this.props;
         let classNames = 'app-list-item d-flex justify-content-between';
 
         if (important) {
@@ -21,20 +21,20 @@ export default class PostListItem extends Component {
                 <span
                     className="app-list-item-label"
                     onClick={onToggleLiked}>
-                    {label}
+                        {label}
                 </span>
                 <div className="d-flex justify-content-center align-items-center">
                     <button
                         type="button"
                         className="btn-star btn-sm"
                         onClick={onToggleImportant}>
-                        <i className="fa fa-star"></i>
+                            <i className="fa fa-star"></i>
                     </button>
                     <button
                         type="button"
                         className="btn-trash btn-sm"
                         onClick={onDelete}>
-                        <i className="fa fa-trash-o"></i>
+                            <i className="fa fa-trash"></i>
                     </button>
                     <i className="fa fa-heart"></i>
                 </div>
